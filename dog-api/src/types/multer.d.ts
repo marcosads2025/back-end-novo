6d2e73cdbd084505ec97718ec29a4b0f9dd1bc1b
marcosads2025 <marcosads2025@gmail.com>
2025-11-26 15:58:54 -0300

    Update multer.d.ts
    correção

--- dog-api/src/types/multer.d.ts
+++ dog-api/src/types/multer.d.ts
@@ -1,1 +1,19 @@
-import multer
+import multer from "multer";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const storage = multer.diskStorage({
+  destination: (_req, _file, cb) => {
+    // pasta uploads na raiz do projeto
+    cb(null, path.resolve(__dirname, "../../uploads"));
+  },
+  filename: (_req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+export const upload = multer({ storage });
+
